7dae9baf286f2956d06d93bd75b60477e1be1f0c
Mr.Lee <120773998+ekgns10000@users.noreply.github.com>
2023-01-28 22:55:24 +0900

    Section 3 - 알티노 라이트 디스플레이 제어하기 [진수]
    2023-01-25 진행
    2023-01-28 복습

---
+++ Book2 Section3/Book2 Section3 Num1-2.py
@@ -0,0 +1,11 @@
+from AltinoLite import*
+
+Open()
+name=[1,2,4,8,16,32,64,128]
+
+for i in range(0,7):
+    
+    displayline(name[i],name[i],name[i],name[i],name[i],name[i],name[i],name[i])
+    delay(1000)
+
+Close()
