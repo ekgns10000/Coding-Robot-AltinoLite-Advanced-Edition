b194b9ffc19483f60b46dffed48cc7f660b50fdc
Mr.Lee <120773998+ekgns10000@users.noreply.github.com>
2023-01-28 22:58:11 +0900

    Section 1 - 데이터와 변수 이해하기
    2023-01-25 진행
    2023-01-28 복습

---
+++ Book2 Section1/Book2 Section1 Num2-1.py
@@ -0,0 +1,14 @@
+from AltinoLite import*
+
+Open()
+
+for i in range(0,10):
+    a=48
+
+    Display(a+i)
+    delay(1000)
+
+Display(0)
+delay(1000)
+
+Close()
---
+++ Book2 Section1/Book2 Section1 Num2-3.py
@@ -0,0 +1,16 @@
+from AltinoLite import*
+
+Open()
+a=48
+i=0
+
+while i<10:
+ 
+    Display(a+i)
+    delay(1000)
+    i=i+1
+
+Display(0)
+delay(1000)
+
+Close()
